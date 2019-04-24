51d7a2a3ab19a2382740bf00e483f1b61195ddf0
Rene van Dijk <renevandijk3@hotmail.com>
2019-04-24 22:10:28 +0200

    add repeat message function

--- routes/firstAired.js
+++ routes/firstAired.js
@@ -11,55 +11,59 @@ router.use(bodyParser.json());
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  let showTitle = req.body.queryResult.parameters.shows; 
-  let url = "http://www.omdbapi.com/?t="+showTitle+"&apikey=752fa0bf";
+  let message = req.body.queryResult.parameters.messageBody; 
+  let recipient = req.body.queryResult.parameters.namen; 
+  // let url = "http://www.omdbapi.com/?t="+showTitle+"&apikey=752fa0bf";
   let action = req.body.queryResult.action; 
-
+ console.log("we did hit the route"); 
   
-    axios.get(url).then(aRes => {
-      let apiData = aRes.data;  
-      let actors =  apiData.Actors; 
-      let writers = apiData.Writer;
-      let title =   apiData.Title; 
-      let image =   apiData.Poster; 
-      let score =   apiData.imdbRating; 
-      let plot =    apiData.Plot;
-      let link =    "https://www.imdb.com/title/"+apiData.imdbID; 
-      let textResponse = ""; 
-      let standardText = " what else can i do for you?"; 
+    // axios.get(url).then(aRes => {
+    //   let apiData = aRes.data;  
+    //   let actors =  apiData.Actors; 
+    //   let writers = apiData.Writer;
+    //   let title =   apiData.Title; 
+    //   let image =   apiData.Poster; 
+    //   let score =   apiData.imdbRating; 
+    //   let plot =    apiData.Plot;
+    //   let link =    "https://www.imdb.com/title/"+apiData.imdbID; 
+    //   let textResponse = ""; 
+    //   let standardText = " what else can i do for you?"; 
 
-      if( action == "actors"){
-         textResponse = "The main actors of "+ title + " are: " + actors; 
-      }else if(action == "writers"){
-        textResponse = "The writers of "+ title + " are: " + writers ; 
-      } else if(action == "score"){
-        textResponse = "IMDB users gave "+ title + " a score of: " + score ; 
-      }else if(action == "plot"){
-        textResponse = plot; 
-      }else {
-      console.log("i could not find what you are looking for.")
-    }
-      res.send(createTextResponse(textResponse, title, image, link, score, plot, standardText)); 
-    }).catch(err =>{
-      console.log(err); 
-    })
+    //   if( action == "actors"){
+    //      textResponse = "The main actors of "+ title + " are: " + actors; 
+    //   }else if(action == "writers"){
+    //     textResponse = "The writers of "+ title + " are: " + writers ; 
+    //   } else if(action == "score"){
+    //     textResponse = "IMDB users gave "+ title + " a score of: " + score ; 
+    //   }else if(action == "plot"){
+    //     textResponse = plot; 
+    //   }else {
+    //   console.log("i could not find what you are looking for.")
+    // }
+    //   
+    // }).catch(err =>{
+    //   console.log(err); 
+    // })
+    textResponse = "uw bericht voor " + recipient + " is: " + message +"." + " is dit correct?"; 
+    res.send(createTextResponse(textResponse)); 
 });
 
 
 
-function createTextResponse (textResponse, title, image, link){
+
+function createTextResponse (textResponse){
   let response = {
-    "fulfillmentText":title,
+    "fulfillmentText":"title",
     "fulfillmentMessages": [
       {
         "card": {
-          "title": title + ":",
+          "title": "title",
           "subtitle": textResponse,
-          "imageUri": image,
+          "imageUri": "image",
           "buttons": [
             {
               "text": "Se more",
-              "postback": link
+              "postback": "link"
             }
           ]
         }
@@ -88,8 +92,9 @@ function createTextResponse (textResponse, title, image, link){
     },
   
   };
-return response; 
+  return response; 
 }
+
 
 module.exports = router;
 
